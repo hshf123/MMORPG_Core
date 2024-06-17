34764ced9f9e2d0ed8346d1c4791d98d916b9694
Sanghyun <hshf123@fowgames.com>
2024-06-17 12:21:47 +0900

    패킷핸들러

---
+++ ServerCore/PacketHandler.cpp
@@ -0,0 +1,43 @@
+#include "pch.h"
+#include "PacketHandler.h"
+
+PacketHandlerFunc PacketHandler::_packetHandler[UINT16_MAX];
+std::unordered_set<uint16> PacketHandler::_useProtocol;
+
+void PacketHandler::Init()
+{
+	_useProtocol.clear();
+	for (int32 i = 0; i < UINT16_MAX; i++)
+		_packetHandler[i] = Handle_INVALID;
+}
+
+bool PacketHandler::HandlePacket(std::shared_ptr<PacketSession>& session, BYTE* buffer, uint32 len)
+{
+	PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+	return _packetHandler[header->id](session, buffer, len);
+}
+
+bool PacketHandler::RegisterHandler(const uint16& protocol, PacketHandlerFunc fn)
+{
+	if (fn == nullptr)
+		return false;
+
+	if (_useProtocol.insert(protocol).second == false)
+	{
+		VIEW_WRITE_ERROR("Duplicated Protocol Detected! Check Protocol ID({})", protocol);
+		return false;
+	}
+
+	_packetHandler[protocol] = fn;
+	return true;
+}
+
+bool Handle_INVALID(std::shared_ptr<PacketSession>& session, BYTE* buffer, int32 len)
+{
+	PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+	if (header == nullptr)
+		return false;
+
+	VIEW_WRITE_ERROR("Invalid Pakcet dected ID({}), IP({})", header->id, StrUtils::ToString(session->GetAddress().GetIpAddress()));
+	return false;
+}
---
+++ ServerCore/PacketHandler.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "Session.h"
+
+using PacketHandlerFunc = std::function<bool(std::shared_ptr<PacketSession>&, BYTE*, int32)>;
+bool Handle_INVALID(std::shared_ptr<PacketSession>& session, BYTE* buffer, int32 len);
+
+class PacketHandler	
+{
+public:
+	static void Init();
+	static bool HandlePacket(std::shared_ptr<PacketSession>& session, BYTE* buffer, uint32 len);
+	static bool RegisterHandler(const uint16& protocol, PacketHandlerFunc fn);
+
+private:
+	template<class PacketType, class ProcessFunc>
+	static bool HandlePacket(ProcessFunc func, std::shared_ptr<PacketSession>& session, BYTE* buffer, int32 len)
+	{
+		PacketType pkt;
+		if (pkt.ParseFromArray(buffer + sizeof(PacketHeader), len - sizeof(PacketHeader)) == false)
+			return false;
+
+		return func(session, pkt);
+	}
+
+	template<class T>
+	static std::shared_ptr<SendBuffer> MakeSendBuffer(T& pkt, uint16 pktId)
+	{
+		const uint16 dataSize = static_cast<uint16>(pkt.ByteSizeLong());
+		const uint16 packetSize = dataSize + sizeof(PacketHeader);
+
+		std::shared_ptr<SendBuffer>  sendBuffer = SendBufferManager::GetInstance().Open(packetSize);
+		PacketHeader* header = reinterpret_cast<PacketHeader*>(sendBuffer->Buffer());
+		header->size = packetSize;
+		header->id = pktId;
+		ASSERT_CRASH(pkt.SerializeToArray(&header[1], dataSize));
+		sendBuffer->Close(packetSize);
+
+		return sendBuffer;
+	}
+
+private:
+	static PacketHandlerFunc _packetHandler[UINT16_MAX];
+	static std::unordered_set<uint16> _useProtocol;
+};
