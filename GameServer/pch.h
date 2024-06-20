bf5579690e8d4eb9f824b1a453c305451ecbe078
Sanghyun <hshf123@fowgames.com>
2024-06-20 10:10:36 +0900

    ignore 수정

--- GameServer/pch.h
+++ GameServer/pch.h
@@ -3,11 +3,7 @@
 #include "CorePch.h"
 
 #pragma comment(lib, "ServerCore.lib")
-#ifdef DEV_TEST
-#pragma comment(lib, "Protobuf\\Debug\\libprotobufd.lib")
-#else
-#pragma comment(lib, "Protobuf\\Release\\libprotobuf.lib")
-#endif
+#pragma comment(lib, "Protobuf\\libprotobuf.lib")
 
 #include "Enum.pb.h"
 #include "Struct.pb.h"
