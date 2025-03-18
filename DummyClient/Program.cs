24aedca2be162dc89712c07c831f5b8f8f8d4bc8
Sanghyun <hshf123@fowgames.com>
2025-03-18 12:02:20 +0900

    더미클라 테스트위해 수정

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -8,7 +8,8 @@ namespace DummyClient
     internal class Program
     {
         static readonly int SetSize = 8096;
-        static readonly int ConnectCount = 14000;
+        static readonly int ConnectCount = 30000;
+        static readonly int ConnectCountInOnce = 300;
         //static int pakcetNum = 0;
 
         static KeyValuePair<float, float>[] targetPosList =
@@ -24,14 +25,14 @@ namespace DummyClient
             Thread.Sleep(1000);
 
             Connector connector = new Connector();
-
+            #region Connect
             int connCount = 0;
             long tick = System.Environment.TickCount64;
             while(connCount < ConnectCount)
             {
                 if (tick > System.Environment.TickCount64)
                     continue;
-                tick = System.Environment.TickCount64 + 1000;
+                tick = System.Environment.TickCount64 + 1500;
 
                 connector.Connect(
                     "127.0.0.1",                                                            // IP
@@ -40,57 +41,63 @@ namespace DummyClient
                     {
                         return ServerSessionManager.Instance.CreateSession();               // Session Create Func Ptr
                     },
-                    300                                                                    // Dummy Client Count
+                    ConnectCountInOnce                                                      // Dummy Client Count
                 );
 
-                connCount += 300;
+                connCount += ConnectCountInOnce;
             }
-
+            #endregion
+            int loopNum = 0;
             while (true)
             {
                 try
                 {
                     #region Echo
-                    //CSChatRequest packet = new CSChatRequest();
-                    //packet.Messageid = EPacketProtocol.CsChatRequest;
-                    //packet.Name = "Client";
-                    //packet.Msg = $"Hello Server {pakcetNum++}";
-                    //await ServerSessionManager.Instance.BroadcastAsync(EPacketProtocol.CsChatRequest, packet);
+                    {
+                        CSChatRequest packet = new CSChatRequest();
+                        packet.Name = "Client";
+                        packet.Msg = "Hello Server!!";
+                        await ServerSessionManager.Instance.BroadcastAsync(EPacketProtocol.CsChatRequest, packet);
+                    }
                     #endregion
                     #region CircularSector
-                    //CSCircularSectorSkillRequest packet = new CSCircularSectorSkillRequest();
-                    //packet.Theta = 60.0f;
-                    //packet.Radius = 5.0f;
-                    //packet.MyPos = new SPosition();
-                    //packet.MyPos.X = 2.0f;
-                    //packet.MyPos.Y = 1.0f;
-                    //packet.Forward = new SPosition();
-                    //packet.Forward.X = 3.0f;
-                    //packet.Forward.Y = 2.0f;
+                    {
+                        CSCircularSectorSkillRequest packet = new CSCircularSectorSkillRequest();
+                        packet.Theta = 60.0f;
+                        packet.Radius = 5.0f;
+                        packet.MyPos = new SPosition();
+                        packet.MyPos.X = 2.0f;
+                        packet.MyPos.Y = 1.0f;
+                        packet.Forward = new SPosition();
+                        packet.Forward.X = 3.0f;
+                        packet.Forward.Y = 2.0f;
 
-                    //KeyValuePair<float, float> pair = targetPosList[pakcetNum++ % 4];
-                    //packet.TargetPos = new SPosition();
-                    //packet.TargetPos.X = pair.Key;
-                    //packet.TargetPos.Y = pair.Value;
-                    //await ServerSessionManager.Instance.BroadcastAsync(EPacketProtocol.CsCircularSectorSkillRequest, packet);
+                        KeyValuePair<float, float> pair = targetPosList[loopNum++ % 4];
+                        packet.TargetPos = new SPosition();
+                        packet.TargetPos.X = pair.Key;
+                        packet.TargetPos.Y = pair.Value;
+                        await ServerSessionManager.Instance.BroadcastAsync(EPacketProtocol.CsCircularSectorSkillRequest, packet);
+                    }
                     #endregion
                     #region Big
-                    int listSize = SetSize / 24;
-
-                    CSBigTestRequest packet = new CSBigTestRequest();
-                    int size = packet.CalculateSize();
-                    for (int i = 0; i < listSize; i++)
                     {
-                        BigTest t = new BigTest();
-                        t.A = Int64.MaxValue;
-                        t.B = Int64.MaxValue;
-                        t.C = Int64.MaxValue;
-                        packet.List.Add(t);
+                        int listSize = SetSize / 24;
+
+                        CSBigTestRequest packet = new CSBigTestRequest();
+                        int size = packet.CalculateSize();
+                        for (int i = 0; i < listSize; i++)
+                        {
+                            BigTest t = new BigTest();
+                            t.A = Int64.MaxValue;
+                            t.B = Int64.MaxValue;
+                            t.C = Int64.MaxValue;
+                            packet.List.Add(t);
+                        }
+
+                        size = packet.CalculateSize();
+
+                        await ServerSessionManager.Instance.BroadcastAsync(EPacketProtocol.CsBigTestRequest, packet);
                     }
-
-                    size = packet.CalculateSize();
-
-                    await ServerSessionManager.Instance.BroadcastAsync(EPacketProtocol.CsBigTestRequest, packet);
                     #endregion
                 }
                 catch (Exception e)
